1b657942c07912ea879d709c0b78ee1698eb4b0e
LeoBertoletti <leonardo.bertoletti@gmail.com>
2023-04-17 15:29:01 -0300

    lista de moedas pronta

---
+++ teste.js
@@ -0,0 +1,79 @@
+const fromAmountInput = document.getElementById('fromAmountInput');
+const toAmountInput = document.getElementById('toAmountInput');
+const MoedaInSelect = document.getElementById('MoedaInSelect');
+const MoedaOutSelect = document.getElementById('MoedaOutSelect');
+
+let taxas = {};
+let MoedaIn = '';
+let MoedaOut = '';
+
+MoedaInSelect.addEventListener('change', handleMudarMoeda);
+MoedaOutSelect.addEventListener('change', handleMudarMoeda);
+fromAmountInput.addEventListener('input', updateConversao);
+
+
+
+
+function inverterMoedas() {
+
+    MoedaIn = MoedaInSelect.value;
+    MoedaOut = MoedaOutSelect.value;
+
+    MoedaInSelect.value = MoedaOut;
+    MoedaOutSelect.value = MoedaIn;
+
+    handleMudarMoeda();
+}
+
+function carregarMoedas(moedas) {
+    const options = Object.keys(moedas);
+    options.forEach(option => {
+        const fromOption = document.createElement('option');
+        const toOption = document.createElement('option');
+        fromOption.textContent = option;
+        toOption.textContent = option;
+        MoedaInSelect.appendChild(fromOption);
+        MoedaOutSelect.appendChild(toOption);
+    });
+}
+
+function handleMudarMoeda() {
+    MoedaIn = MoedaInSelect.value;
+    MoedaOut = MoedaOutSelect.value;
+
+    if (MoedaIn === MoedaOut) {
+        toAmountInput.value = fromAmountInput.value;
+        return;
+    }
+
+    updateConversao();
+}
+
+
+function updateConversao() {
+    const fromAmount = fromAmountInput.value;
+
+    if (!taxas || !taxas[MoedaIn] || !taxas[MoedaOut]) {
+        console.error('Taxas de câmbio não carregadas corretamente.');
+        return;
+    }
+
+    const taxaIn = taxas[MoedaIn];
+    const taxaOut = taxas[MoedaOut];
+    const toAmount = fromAmount * (taxaOut / taxaIn);
+
+    toAmountInput.value = toAmount.toFixed(2);
+}
+
+
+// Chama a API da Open Exchange Rates para obter as opções de moedas e as taxas de câmbio
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        taxas = data.rates;
+        carregarMoedas(taxas);
+        handleMudarMoeda();
+    })
+    .catch(error => {
+        console.error('Erro ao obter as moedas:', error);
+    });

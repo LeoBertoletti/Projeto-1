acaab41b71e8331b6b840aeac74001ae15bd5f4e
LeoBertoletti <leonardo.bertoletti@gmail.com>
2023-04-17 15:29:08 -0300

    lista moedas pronta

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const URL = "https://openexchangerates.org/api/latest.json?app_id=082ffb407a9d4a75a14e9ffcab14a77e";
+const inValue = document.getElementById('inValue')
+const outValue = document.getElementById("outValue")
+const inCurrency = document.getElementById("inCurrency")
+const outCurrency = document.getElementById("outCurrency")
 
 const getData = async () => {
     const response = await fetch(URL);
@@ -12,12 +16,19 @@ const conversor = (iso1, iso2, valor) => {
     return (dataGlobal.rates[iso2] * valor) / dataGlobal.rates[iso1]
 }
 
+const loadMoedas = (moedas) => {
+    const options = Object.keys(moedas);
+    options.forEach(option => {
+        const fromOption = document.createElement('option');
+        fromOption.textContent = option;
+        listaMoedas.appendChild(fromOption);
+    });
+}
+
+
 (async () => {
-    //await getData();
-    const valor1 = document.getElementById("valor1")
-    const valor2 = document.getElementById("valor2")
-    const moeda1 = document.getElementById("moeda1")
-    const moeda2 = document.getElementById("moeda2")
-    const botao = document.getElementById("botao")
+    await getData();
+    loadMoedas(dataGlobal.rates);
+
 
 })();
